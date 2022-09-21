49bbadb8ac2bc88b6562f16a0a864c2916311382
loggerboy9325 <loggerboy9325@gmail.com>
2022-09-21 14:11:44 -0400

    bucket

--- s3.tf
+++ s3.tf
@@ -16,4 +16,8 @@ resource "aws_s3_bucket_object" "object" {
   bucket = "terraformtest3434243324"
   key    = "new_object_key"
   source = "indexupdated.html"
+
+ depends_on = [
+    aws_s3_bucket.b
+  ]
 }
