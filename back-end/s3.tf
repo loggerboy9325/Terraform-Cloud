e6a0cda0e923fd4660d8e3da592ce28e245ba82f
loggerboy9325 <loggerboy9325@gmail.com>
2022-09-22 09:20:47 -0400

    fixed images

--- back-end/s3.tf
+++ back-end/s3.tf
@@ -45,14 +45,3 @@ resource "aws_s3_object" "ccp" {
 
     }
 
-     resource "aws_s3_object" "ssa" {
-  bucket = var.bucket
-  key    = "ssa"
-  source = "front-end/images/SAA.jpg"
-     }
-
- resource "aws_s3_object" "sap" {
-  bucket = var.bucket
-  key    = "SAP"
-  source = "front-end/images/SAP.jpg"
- }
