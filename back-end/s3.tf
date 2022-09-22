6174297bfb273142ce704984c1408bbde0102f27
loggerboy9325 <loggerboy9325@gmail.com>
2022-09-21 22:49:54 -0400

    change

--- back-end/s3.tf
+++ back-end/s3.tf
@@ -45,23 +45,3 @@ resource "aws_s3_object" "ccp" {
 
     }
 
-resource "aws_s3_bucket_website_configuration" "websitebucket" {
-  bucket = var.bucket
-
-  index_document {
-    suffix = "front-end/index.html"
-  }
-
-  error_document {
-    key = "front-end/404.html"
-  }
-
-  routing_rule {
-    condition {
-      key_prefix_equals = "docs/"
-    }
-    redirect {
-      replace_key_prefix_with = "documents/"
-    }
-  }
-}
