31a11d01d521762cddfc3222f76b19293724ab42
loggerboy9325 <loggerboy9325@gmail.com>
2022-10-04 15:19:55 -0400

    changed files

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -3,7 +3,7 @@
 
 
 resource "aws_dynamodb_table" "resume_table" {
-  name           = "GameScores"
+  name           = "visitors"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "ID"
   
--- terraform/lambda_function.py
+++ terraform/lambda_function.py
@@ -2,7 +2,7 @@ import boto3
 import json
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('GameScores')
+table = dynamodb.Table('visitors')
 
 def lambda_handler(event, context):
    
